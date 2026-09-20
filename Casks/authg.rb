da0fbaa6dd2179a0698b7c84a47069986d36859d
Abhinav Dhakal <dhakalabhinav4@gmail.com>
2026-09-20 10:24:27 +0545

    chore(cask): bump to v1.0.5

--- Casks/authg.rb
+++ Casks/authg.rb
@@ -1,14 +1,14 @@
 cask "authg" do
-  version "1.0.4"
+  version "1.0.5"
 
   if Hardware::CPU.arm?
     url "https://github.com/abhidhakal/authg-app/releases/download/v#{version}/AuthG_#{version}_aarch64.dmg",
         verified: "github.com/abhidhakal/authg-app/"
-    sha256 "c06b176b4914c6b76a92908c97a1d55dba925e863f1ff274666693e6f8ea2732"
+    sha256 "dc2c36777aa54ac9caa48c7d2516dc350823212675b1c51b780cc7a6b2cf22c5"
   else
     url "https://github.com/abhidhakal/authg-app/releases/download/v#{version}/AuthG_#{version}_x64.dmg",
         verified: "github.com/abhidhakal/authg-app/"
-    sha256 "84373df2212f64c9a74a91b81462b0140b5888d4378cfc3532517a1dd467f91d"
+    sha256 "fb36e4158f93c211bf3b8431411e7275c5f93bb74003e8c27e99ae9dc8a6e24a"
   end
 
   name "AuthG"
