25d915b80de952fb322f8e6db39eca7c68673970
Abhinav Dhakal <dhakalabhinav4@gmail.com>
2026-09-20 15:06:46 +0545

    fix(cask): modernize syntax for big_sur and v1.0.6

--- Casks/authg.rb
+++ Casks/authg.rb
@@ -1,21 +1,19 @@
 cask "authg" do
-  version "1.0.5"
+  version "1.0.6"
 
   if Hardware::CPU.arm?
-    url "https://github.com/abhidhakal/authg-app/releases/download/v#{version}/AuthG_#{version}_aarch64.dmg",
-        verified: "github.com/abhidhakal/authg-app/"
-    sha256 "dc2c36777aa54ac9caa48c7d2516dc350823212675b1c51b780cc7a6b2cf22c5"
+    url "https://github.com/abhidhakal/authg-app/releases/download/v#{version}/AuthG_#{version}_aarch64.dmg"
+    sha256 "63a2aae47318e3f412c28042de8b32d06d5b22b6b678daeaa8a18404c6c25244"
   else
-    url "https://github.com/abhidhakal/authg-app/releases/download/v#{version}/AuthG_#{version}_x64.dmg",
-        verified: "github.com/abhidhakal/authg-app/"
-    sha256 "fb36e4158f93c211bf3b8431411e7275c5f93bb74003e8c27e99ae9dc8a6e24a"
+    url "https://github.com/abhidhakal/authg-app/releases/download/v#{version}/AuthG_#{version}_x64.dmg"
+    sha256 "9461afc7fb4fca75112b0ca23bac414cd435e4ae538c488ec45b18853f5f3ea0"
   end
 
   name "AuthG"
   desc "Open-source, offline desktop 2FA authenticator for the menu bar"
   homepage "https://authg.abhinavdhakal.com"
 
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :big_sur"
 
   app "AuthG.app"
 
